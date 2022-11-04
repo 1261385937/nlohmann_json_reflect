77020ebb50778eac902db188e5ebd027a503517e
wangwei <1261385937@qq.com>
2022-11-04 22:15:07 +0800

    detect std container nest layer

--- reflect_meta.hpp
+++ reflect_meta.hpp
@@ -168,4 +168,27 @@ struct is_has_reflect_type<T, std::enable_if_t<is_associative_std_container_v<T>
 
 template<typename T>
 inline constexpr bool is_has_reflect_type_v = is_has_reflect_type<T>::value;
+
+//detect std container nest layer
+template<typename T, size_t Layer = 0, typename U = void>
+struct is_nested_sequence :std::false_type {
+    using innermost_type = T;
+    constexpr static size_t layer = Layer;
+};
+
+template<typename T, size_t Layer, typename U = void>
+struct is_nested_sequence :std::true_type {
+    using innermost_type = T;
+    constexpr static size_t layer = Layer;
+};
+
+template<typename T, size_t Layer>
+struct is_nested_sequence<T, Layer, std::enable_if_t<is_sequence_std_container_v<T>>>
+    : is_nested_sequence<typename T::value_type, Layer + 1> {};
+
+template<typename T>
+inline constexpr size_t nested_sequence_layer_v = is_nested_sequence<T>::layer;
+
+template<typename T>
+using nested_sequence_type_t = typename is_nested_sequence<T>::innermost_type;
 }
